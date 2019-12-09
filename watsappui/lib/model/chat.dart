cee286578cac7dc45a8a31c4a70f4e6125938c10
satyam <sat.tiwari21@gmail.com>
2019-12-09 18:06:59 +0530

    first commit

---
+++ watsappui/lib/model/chat.dart
@@ -0,0 +1,61 @@
+class chat {
+  final String name;
+  String message;
+  final String time;
+  final String imageUrl;
+
+  chat({this.name, this.message, this.time, this.imageUrl});
+}
+
+List<chat> messageData = [
+  new chat(
+      name: "Danish Ran",
+      message: "Flutter is all about Widget",
+      time: "10:12",
+      imageUrl:
+          "https://images.pexels.com/photos/1149022/pexels-photo-1149022.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"),
+  new chat(
+      name: "Satyam Roy",
+      message: "welcome to flutter",
+      time: "02:16",
+      imageUrl:
+          "https://images.pexels.com/photos/1427288/pexels-photo-1427288.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"),
+  new chat(
+      name: "Gokul",
+      message:
+          "Flutter is all about Widget",
+      time: "11:11",
+      imageUrl:
+          "https://images.pexels.com/photos/2108706/pexels-photo-2108706.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"),
+  new chat(
+      name: "Nitesh",
+      message: "Python ",
+      time: "20:21",
+      imageUrl:
+          "https://images.pexels.com/photos/1650281/pexels-photo-1650281.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"),
+  new chat(
+      name: "Kaushal Jha",
+      message: "TheFlutter is all about Widget",
+      time: "1:12",
+      imageUrl:
+          "https://images.pexels.com/photos/1397364/pexels-photo-1397364.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"),
+  new chat(
+      name: "Raushan Jha",
+      message:
+          "The dark editor is much better",
+      time: "11:11",
+      imageUrl:
+          "https://images.pexels.com/photos/2108706/pexels-photo-2108706.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"),
+  new chat(
+      name: "Nitesh",
+      message: "Python ",
+      time: "20:21",
+      imageUrl:
+          "https://images.pexels.com/photos/1650281/pexels-photo-1650281.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"),
+  new chat(
+      name: "Kaushal Jha",
+      message: "Flutter is all about Widget",
+      time: "1:12",
+      imageUrl:
+          "https://images.pexels.com/photos/1397364/pexels-photo-1397364.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500")
+];
